38ad44ed2e976c1223c51863eee27c53fd957ef0
loikaa <arseni.loika@usi.ch>
2024-03-09 09:17:24 +0100

    color correction semi working

--- DIP_ASSIGNMENT_1/color_correction.m
+++ DIP_ASSIGNMENT_1/color_correction.m
@@ -1,7 +1,31 @@
 wb_pic = imread("white_balance_input.jpg");
-
 wb_pic = im2double(wb_pic);
-
 imshow(wb_pic);
 
-% [y, x] = find(wb_pic > 100);`
+% select sample pixel
+[x, y, rgb] = impixel(wb_pic);
+% [d,c] = ginput(wb_pic);
+
+
+% split whole image into channels
+% R = wb_pic(:,:,1); 
+% G = wb_pic(:,:,2); 
+% B = wb_pic(:,:,3);
+
+% equalize the rgb values
+r_new = (rgb(1) + rgb(2) + rgb(3)) / 3;
+g_new = (rgb(1) + rgb(2) + rgb(3)) / 3;
+b_new = (rgb(1) + rgb(2) + rgb(3)) / 3;
+
+% find alpha
+alpha_R = r_new/(3*rgb(1));
+alpha_G = g_new/(3*rgb(2));
+alpha_B = b_new/(3*rgb(2));
+
+% process every pixel's corresponding channels
+wb_pic(:,:,1) = wb_pic(:,:,1)*alpha_R;
+wb_pic(:,:,2) = wb_pic(:,:,2)*alpha_G;
+wb_pic(:,:,3) = wb_pic(:,:,3)*alpha_B;
+
+% do I need 
+imshow(wb_pic);
---
+++ DIP_ASSIGNMENT_1/color_correction_grey_world.m
@@ -0,0 +1,2 @@
+% on average, the world is grey
+% get the average RGB value, make it grey
