df091370a30d565a808d0ceb272518973744d581
loikaa <arseni.loika@usi.ch>
2024-04-29 10:47:17 +0200

    ex1 refinements

--- DIP_ASSIGNMENT_3/solutions/gauss.m
+++ DIP_ASSIGNMENT_3/solutions/gauss.m
@@ -1,7 +1,7 @@
 close all;
 
+% Make image
 dim = 1024;
-
 img = ones(dim, dim);
 img = img .*0.9;
 center = dim/2;
@@ -15,8 +15,45 @@ img(center:center+R, center-R:center) = 0;
 % Spatial domain
 % img = padarray(img, [10, 10], 0, 'both');
 
-sigma = 2;
+sigma = 50;
 I_blur = imgaussfilt(img, sigma);
 
+% imshow(I_blur);
 
-imshow(img);
+% Frequency domain
+% compute FT
+I_ft = fft2(img);
+
+% visualize shifted FT
+vI=abs(fftshift(I_ft));
+
+% imshow(vI/100);
+
+% compute gaussian (low-pass) filter
+[X,Y]= meshgrid(-512:511,-512:511);
+D = sqrt(X.^2 + Y.^2);
+D = D/max(D(:));
+term = 1 / 2*pi*sigma;
+D = exp(-D.^2 / 2*sigma^2);
+D = D * term;
+D = D/max(D(:));
+
+% imshow(D)
+
+I_ft_filtered= I_ft .* fftshift(D);
+I_out = ifft2(I_ft_filtered);
+
+figure; 
+tiledlayout(1,2);
+
+nexttile;
+imshow(I_blur); title("Spatial domain Gaussian blur, \sigma=" + sigma);
+
+nexttile;
+imshow(abs(I_out)); title("Frequency domain Gaussian blur, \sigma=" + sigma);
+
+
+
+
+
+
