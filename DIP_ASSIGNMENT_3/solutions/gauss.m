1c49c8f529b3e8c6008bb0f3ccc49a8541cb42a0
loikaa <arseni.loika@usi.ch>
2024-05-07 14:39:26 +0200

    hw2 finalized

--- DIP_ASSIGNMENT_3/solutions/gauss.m
+++ DIP_ASSIGNMENT_3/solutions/gauss.m
@@ -14,7 +14,6 @@ img(center:center+R, center-R:center) = 0;
 
 % Spatial domain
 img = padarray(img, [10, 10], 0, 'both');
-
 sigma = 10;
 I_blur = imgaussfilt(img, sigma);
 
@@ -22,33 +21,43 @@ I_blur = imgaussfilt(img, sigma);
 % compute FT
 I_ft = fft2(img);
 
-% visualize shifted FT
-% vI=abs(fftshift(I_ft));
-% imshow(vI/100); title("fourier");
-
 % compute gaussian (low-pass) filter
 [X,Y]= meshgrid(-522:521,-522:521);
 D = sqrt(X.^2 + Y.^2);
-D = D/max(D(:)); 
+D = D/max(D(:));
 sigma_freq = (1 / (2*pi*sigma));
 D = exp(-D.^2/2/sigma_freq^2);
 D = D/max(D(:));
 
-% imshow(D); title("kernel");
-
 I_ft_filtered= I_ft .* fftshift(D);
-
-% imshow(abs(I_ft_filtered)); title("filtered fourier");
 I_out = ifft2(I_ft_filtered);
 
 % visualize
 figure; 
-tiledlayout(1,2);
+tiledlayout(3,2);
 
+nexttile;
+imshow(img); title("Original");
+
+% shifted FT
+vI=abs(fftshift(I_ft));
+nexttile;
+imshow(vI/100); title("Fourier transform");
+
+% kernel
+nexttile;
+imshow(D); title("Kernel");
+
+% FT * kernel
+nexttile;
+imshow(abs(fftshift(I_ft_filtered))); title("filtered fourier");
+
+% spatial result
 nexttile;
 imshow(I_blur); title("Spatial domain Gaussian blur, \sigma=" + sigma);
 
+% frequency result
 nexttile;
-imshow(abs(I_out)); title("Frequency domain Gaussian blur, \sigma=" + sigma);
+imshow(abs(I_out)); title("Frequency domain Gaussian blur, \sigma=" + sigma_freq);
 
 
--- DIP_ASSIGNMENT_3/solutions/restore.m
+++ DIP_ASSIGNMENT_3/solutions/restore.m
@@ -1,20 +1,20 @@
 close all;
 
-img = imread("san_domenico.png");
+% img = imread("san_domenico.png");
 % img = imread("moon.png");
+% img = imread("AI1.jpeg");
+img = imread("ai_image.jpeg");
+
 img = im2double(img);
 img_f = fft2(img);
 [height, width, ~] = size(img);
 
-% show FT of img
-% imshow(abs(img_f));
-
-% get pixels coords
+factor = height*2;
+% get pixels coords - SELECT ARTIFACTS MANUALLY
 [x, y, ~] = impixel(abs(fftshift(img_f)/500));
 
 % make the filter
 mask = ones(height, width);
-
 % radius
 R = 15;
 % make circular filter
@@ -23,16 +23,30 @@ for i = 1:size(x)
     mask((h-x(i)).^2+(w-y(i)).^2<R^2)=0;
 end
 
-% show mask
-% imshow(mask);
-
 % apply filter to the frequency domain
 res_f = img_f .* fftshift(mask);
 
-% show result of filter on frequency domain
-% imshow(abs(fftshift(res_f)/500));
-
 % apply inverse fft
 out = abs(ifft2(res_f));
-% imshow(out); 
 
+% visualize
+tiledlayout(2,2);
+
+% show img
+imshow(img); title("Original");
+
+% show FT of img
+nexttile;
+imshow(abs(fftshift(img_f)/500)); title("Image FT");
+
+% show mask
+nexttile;
+imshow(mask); title("Mask");
+
+% show result of filter on frequency domain
+nexttile;
+imshow(abs(fftshift(res_f)/500)); title("Result of filter on Frequency Domain");
+
+% show result
+nexttile;
+imshow(out); title("Inverse FT of image's filtered frequency domain");
