4e178c1e2e1aa2bb8372105b308d0f4a8e12c132
loikaa <arseni.loika@usi.ch>
2024-05-16 12:24:52 +0200

    e1.1 done

--- DIP_ASSIGNMENT_4/solutions/RGBPalette.m
+++ DIP_ASSIGNMENT_4/solutions/RGBPalette.m
@@ -1,6 +1,7 @@
 close all;
 
 img =  im2double(imread("../queen.jpg"));
+
 img_lin = img.^2.2;
 [height, width, ~] = size(img);
 
@@ -11,7 +12,7 @@ blueC= img_lin(:,:,3);
 data = double([redC(:), blueC(:), greenC(:)]);
 
 % perform kmeans on 7 clusters
-[idx, centroids] = kmeans(data, 7, Replicates=1);
+[idx, centroids] = kmeans(data, 7, Replicates=2);
 
 % make a cell array of classes
 classes = cell(1,7);
@@ -30,28 +31,27 @@ for i = 1:length(classes)
     out_green= cur .* img_lin(:,:,2);
     out_blue= cur .* img_lin(:,:,3);
     pic_out = cat(3, out_red, out_green, out_blue);
+
     pics{i} = pic_out;
-%     pics{i} = rgb2hsv(pic_out);
     
     out_red(out_red == 0) = NaN;
     out_green(out_green == 0) = NaN;
     out_blue(out_blue == 0) = NaN;
 
     out = cat(3, out_red, out_green, out_blue);
+    out = rgb2hsv(out);
+    
     res{i} = out;
 end
 
 % get the average values
 avg_list = cell(1,7);
 for i = 1:length(res)
-    avg_red = mean(res{i}(:,:,1), "all", "omitnan");
-    avg_green = mean(res{i}(:,:,2), "all", "omitnan");
-    avg_blue = mean(res{i}(:,:,3), "all", "omitnan");
-
     wdw = img_lin;
     wdw(:,:,1) = centroids(i,1);
     wdw(:,:,2) = centroids(i,3); % this bothers me a lot
     wdw(:,:,3) = centroids(i,2);
+
     avg_list{i} = wdw;
 end
 
