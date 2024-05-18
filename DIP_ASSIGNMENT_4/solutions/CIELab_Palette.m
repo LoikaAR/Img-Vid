1d3b9e8f406276b36e30a87b0b9a143dd64fcb8b
loikaa <arseni.loika@usi.ch>
2024-05-18 13:15:42 +0200

    ex1.2 done

---
+++ DIP_ASSIGNMENT_4/solutions/CIELab_Palette.m
@@ -0,0 +1,54 @@
+close all;
+
+img = imread("../queen.jpg");
+img = im2double(img);
+
+img_lin = rgb2lab(img);
+
+[height, width, ~] = size(img);
+
+% prepare data
+redC = img_lin(:,:,1);
+greenC= img_lin(:,:,2);
+blueC= img_lin(:,:,3);
+data = double([redC(:), blueC(:), greenC(:)]);
+
+% perform kmeans on 7 clusters
+[idx, centroids] = kmeans(data, 7, Replicates=5);
+
+% make a cell array of classes
+classes = cell(1,7);
+for i =1:7
+    class = reshape(idx==i,height,width);
+    classes{i} = class;
+end
+
+% show pixels of img_lin where class1,2,3,... == 1
+pics = cell(1,7);
+res = cell(1,7);
+for i = 1:length(classes)
+    cur = classes{i};
+    
+    out_red = cur .* img_lin(:,:,1);
+    out_green= cur .* img_lin(:,:,2);
+    out_blue= cur .* img_lin(:,:,3);
+    pic_out = cat(3, out_red, out_green, out_blue); 
+    pic_out = lab2rgb(pic_out);
+
+    pics{i} = pic_out;
+    res{i} = pic_out;
+end
+
+% get the average values
+avg_list = cell(1,7);
+for i = 1:length(res)
+    wdw = img_lin;
+    wdw(:,:,1) = centroids(i,1);
+    wdw(:,:,2) = centroids(i,3); % this bothers me a lot
+    wdw(:,:,3) = centroids(i,2);
+
+    avg_list{i} = lab2rgb(wdw);
+end
+
+collated = [pics, avg_list];
+montage(collated,Size=[2 NaN],BorderSize=[2,2],BackgroundColor=[1,1,1]);
---
+++ DIP_ASSIGNMENT_4/solutions/RGB_Palette.m
@@ -0,0 +1,60 @@
+close all;
+
+img =  im2double(imread("../queen.jpg"));
+% img =  im2double(imread("../color.jpg"));
+img_lin = img.^2.2;
+[height, width, ~] = size(img);
+
+% prepare data
+redC = img_lin(:,:,1);
+greenC= img_lin(:,:,2);
+blueC= img_lin(:,:,3);
+data = double([redC(:), blueC(:), greenC(:)]);
+
+% perform kmeans on 7 clusters
+[idx, centroids] = kmeans(data, 7, Replicates=2);
+
+% make a cell array of classes
+classes = cell(1,7);
+for i =1:7
+    class = reshape(idx==i,height,width);
+    classes{i} = class;
+end
+
+% show pixels of img_lin where class1,2,3,... == 1
+pics = cell(1,7);
+res = cell(1,7);
+for i = 1:length(classes)
+    cur = classes{i};
+    
+    % reconstruct the colors
+    out_red = cur .* img_lin(:,:,1);
+    out_green= cur .* img_lin(:,:,2);
+    out_blue= cur .* img_lin(:,:,3);
+    pic_col = cat(3, out_red, out_green, out_blue);
+    
+    % split and modify hue, sat, value
+    [H, S, V] = rgb2hsv(pic_col);
+    % edit here ...
+
+    pic_out = cat(3, H, S, V);
+    pic_out = hsv2rgb(pic_out);
+    pics{i} = pic_out;
+
+    out = cat(3, out_red, out_green, out_blue);
+    res{i} = out;
+end
+
+% get the average values
+avg_list = cell(1,7);
+for i = 1:length(res)
+    wdw = img_lin;
+    wdw(:,:,1) = centroids(i,1);
+    wdw(:,:,2) = centroids(i,3); % this bothers me a lot
+    wdw(:,:,3) = centroids(i,2);
+
+    avg_list{i} = wdw;
+end
+
+collated = [pics, avg_list];
+montage(collated,Size=[2 NaN],BorderSize=[2,2],BackgroundColor=[1,1,1]);
